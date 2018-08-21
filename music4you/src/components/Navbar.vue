cb0f6157b9b0cbbe037e0302e75f2433ccfdde0a
Etienne Roulet <etienne.roulet@gibmit.ch>
2018-08-21 16:35:39 +0200

    sample search

--- music4you/src/components/Navbar.vue
+++ music4you/src/components/Navbar.vue
@@ -32,13 +32,47 @@
     data () {
       return {
         activeIndex: '1',
-        activeIndex2: '1'
+        activeIndex2: '1',
+        links: [],
+        state4: '',
+        timeout: null
       }
     },
     methods: {
-      handleSelect (key, keyPath) {
+      handleSelects (key, keyPath) {
         console.log(key, keyPath)
+      },
+      loadAll () {
+        return [
+          { 'value': 'vue', 'link': 'https://github.com/vuejs/vue' },
+          { 'value': 'element', 'link': 'https://github.com/ElemeFE/element' },
+          { 'value': 'cooking', 'link': 'https://github.com/ElemeFE/cooking' },
+          { 'value': 'mint-ui', 'link': 'https://github.com/ElemeFE/mint-ui' },
+          { 'value': 'vuex', 'link': 'https://github.com/vuejs/vuex' },
+          { 'value': 'vue-router', 'link': 'https://github.com/vuejs/vue-router' },
+          { 'value': 'babel', 'link': 'https://github.com/babel/babel' }
+        ]
+      },
+      querySearchAsync (queryString, cb) {
+        var links = this.links
+        var results = queryString ? links.filter(this.createFilter(queryString)) : links
+
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => {
+          cb(results)
+        }, 3000 * Math.random())
+      },
+      createFilter (queryString) {
+        return (link) => {
+          return (link.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+        }
+      },
+      handleSelect (item) {
+        console.log(item)
       }
+    },
+    mounted () {
+      this.links = this.loadAll()
     }
   }
 </script>
