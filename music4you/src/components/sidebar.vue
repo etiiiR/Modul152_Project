7fac53b092248e43ebc779bc765666da6f93f709
Etienne Roulet <etienne.roulet@gibmit.ch>
2018-08-21 10:42:15 +0200

    Css fix

--- music4you/src/components/sidebar.vue
+++ music4you/src/components/sidebar.vue
@@ -5,35 +5,21 @@
             <img class="el-logo" v-bind:src='img' height="30px">
             <span slot="title">Music4You</span>
         </el-menu-item>
-        <el-submenu index="2">
-            <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title">alle Title</span>
-            </template>
-            <el-menu-item-group>
-                <span slot="title">Group One</span>
-                <el-menu-item index="2-1">item one</el-menu-item>
-                <el-menu-item index="2-2">item two</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-                <el-menu-item index="2-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="2-4">
-                <span slot="title">item four</span>
-                <el-menu-item index="2-4-1">item one</el-menu-item>
-            </el-submenu>
-        </el-submenu>
+        <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">Künstler</span>
+        </el-menu-item>
         <el-menu-item index="3">
             <i class="el-icon-menu"></i>
             <span slot="title">Künstler</span>
         </el-menu-item>
         <el-menu-item index="4" disabled>
             <i class="el-icon-document"></i>
-            <span slot="title">Genere</span>
+            <span slot="title">Generes</span>
         </el-menu-item>
         <el-menu-item index="5">
             <i class="el-icon-setting"></i>
-            <span slot="title">Top Hits</span>
+            <span slot="title">Uploade</span>
         </el-menu-item>
         <el-menu-item index="6">
             <i class="el-icon-setting"></i>
