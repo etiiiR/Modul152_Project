63b2535e01b70a3fa4ea5a15834cb85d7d94d977
Etienne Roulet <etienne.roulet@gibmit.ch>
2018-09-05 18:31:35 +0200

    commit fix login with token
    token

--- music4you/src/components/Login.vue
+++ music4you/src/components/Login.vue
@@ -32,22 +32,23 @@
     },
     methods: {
       onSubmit () {
-        alert(this.form.name, this.form.password, this.form.email)
-        console.log( "username:", this.form.name, "email:", this.form.email, "password:", this.form.password, )
         this.$http.post('/rest-auth/login/', { username: this.form.name, email: this.form.email, password: this.form.password, })
         .then(request => this.loginSuccessful(request))
         .catch(() => this.loginFailed())
       },
       loginSuccessful (req) {
         if (!req.data.token) {
+          alert("loggin faild trow login sucessful")
           this.loginFailed()
           return
         }
         localStorage.token = req.data.token
         this.error = false
+        alert("loginsucessfull")
         this.$router.replace(this.$route.query.redirect || '/authors')
       },
       loginFailed () {
+        alert("loggin failed")
         this.error = 'Login failed!'
         delete localStorage.token
       }
